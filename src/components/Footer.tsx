af6ec1bbe7120a5d199d18dedf7a2c64e0558a42
Asif Ali Mir <asif15310@gmail.com>
2024-07-04 09:26:58 +0530

    fixed gradient

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,20 +4,20 @@ import { FaArrowUp } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="bg-gradient-to-r from-[#d3e97a] via-[#222222] to-[#d3e97a] text-black pt-8 pb-4 sm:pt-16 relative">
+        <footer className="bg-gradient-to-r from-[#d3e97a] via-[#222222] to-[#d3e97a] text-[#c7c7c7] pt-16 pb-4 sm:pt-24 relative">
             <div className="container mx-auto px-4 w-full max-w-full space-y-6 sm:max-w-screen-xl">
                 <div className="flex flex-col md:flex-row justify-between items-center">
                     <div className="mb-4 md:mb-0 text-center md:text-left">
-                        <h2 className="text-2xl font-bold font-amenti text-black">Aasif Ali</h2>
-                        <p className="text-black font-amenti font-regular">Web & App Developer</p>
+                        <h2 className="text-2xl font-bold font-amenti text-[#c7c7c7]">Aasif Ali</h2>
+                        <p className="text-[#c7c7c7] font-amenti font-regular">Web & App Developer</p>
                     </div>
 
                     <div className="flex flex-col gap-4 ">
                         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 text-center md:text-left">
-                            <a href="#about" className="hover:text-gray-300 text-black font-medium font-amenti">About Me</a>
-                            <a href="#contact" className="hover:text-gray-300 text-black font-medium font-amenti">Contact Me</a>
-                            <a href="tel:+91788942564" className="hover:text-gray-300 text-black font-medium font-amenti">+91,7889423564</a>
-                            <a href="resume.pdf" className="hover:text-gray-300 text-black font-medium font-amenti">Resume</a>
+                            <a href="#about" className="hover:text-gray-300 text-[#c7c7c7] font-medium font-amenti">About Me</a>
+                            <a href="#contact" className="hover:text-gray-300 text-[#c7c7c7]  font-medium font-amenti">Contact Me</a>
+                            <a href="tel:+91788942564" className="hover:text-gray-300 text-[#c7c7c7] font-medium font-amenti">+91,7889423564</a>
+                            <a href="resume.pdf" className="hover:text-gray-300 text-[#c7c7c7] font-medium font-amenti">Resume</a>
                         </div>
                         <div className="flex flex-col sm:flex-row items-center gap-2">
                             <div>
@@ -38,7 +38,7 @@ const Footer = () => {
                     </div>
 
                 </div>
-                <div className="mt-4 text-center text-black font-medium text-xs font-amenti">
+                <div className="mt-4 text-center text-[#c7c7c7] font-medium text-xs font-amenti">
                     <p>&copy; {new Date().getFullYear()} Aasif Ali. All rights reserved.</p>
                 </div>
             </div>
